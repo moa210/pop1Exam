e8c64faaf796b44f17f8054eaad0fcbfdaaa2161
moa210 <moa21@o2.pl>
2020-06-03 12:45:24 +0100

    some exercises done

--- Main.py
+++ Main.py
@@ -1,4 +1,7 @@
-def number_of_differences(n, m, A, B):
+import math
+
+
+def number_of_differences(n: object, m: object, A: object, B: object) -> object:
     diff = 0
     # j: int
     # i: int
@@ -11,33 +14,71 @@ def number_of_differences(n, m, A, B):
     return diff
 
 
+# my_set = {"one", "two", "tree"}
+# print( my_set)
+# # we discuss sets in detail later
+#
+# new_set = my_set.difference({"one", "two"})
+# print(new_set)
+#
+# my_set.difference_update({"one", "two"})
+# print(my_set)
+
 assert number_of_differences(2, 3, [[1, 2, 3], [4, 5, 6]], [[1, 2, 4], [3, 5, 6]]) == 2
 assert number_of_differences(2, 2, [[1, 2], [3, 4]], [[1, 2], [3, 4]]) == 0
 
 
-def generate_sentences(subjects, predicates, objects):
-    new_str_s = [""]
-    for i in range(len(subjects)):
-        for j in range(len(predicates)):
-            for k in range(len(objects)):
-                new_str_s.append(f"{subjects[i]} {predicates[j]} {objects[k]}.")
+class Airplane:
+    def __init__(self, initX, initY, cons, init_fuel):
+        self.fuel_level = init_fuel
+        self.position = [initX, initY]
+        self.consumption = cons
 
-    new_str_s.sort()
-    sorted_str = " ".join(new_str_s)
-    print(sorted_str)
-    return sorted_str
+    def goto(self, X, Y):
+        distance = math.sqrt(((X - self.position[0] ) ** 2) + (Y - self.position[1]) ** 2)
+        fuel_needed = self.consumption * distance
+        if fuel_needed < self.fuel_level:
+            self.fuel_level -= fuel_needed
+            self.position[0] = X
+            self.position[1]= Y
+            return True
+        else:
+            return False
 
 
-assert generate_sentences(["John", "Mary"], ["hates", "loves"],
-                          ["apples", "bananas"]) == "John hates apples. John hates bananas. " \
-                                                    "John loves apples. John loves bananas. " \
-                                                    "Mary hates apples. Mary hates bananas. " \
-                                                    "Mary loves apples. Mary loves bananas."
+def refuel(self, fuel):
+    self.fuel += fuel
 
-assert generate_sentences(["Vlad", "Hubie"], ["drives"],
-                          ["car", "motorcycle", "bus"]) == "Hubie drives bus. " \
-                                                       "Hubie drives car. " \
-                                                       "Hubie drives motorcycle. " \
-                                                       "Vlad drives bus. " \
-                                                       "Vlad drives car. " \
-                                                       "Vlad drives motorcycle."
+
+ap789 = Airplane(0, 0, 10, 3000)
+assert ap789.goto(95, 70) == True
+assert ap789.position[0] == 95 and ap789.position[1] == 70
+# print(ap789.fuel_level - 1819.96)
+assert abs(ap789.fuel_level - 1819.96) < 0.01
+
+
+def longest_sequence(param):
+    longest_so_far = 1
+    letter = param[0]
+    smallest_letter = param[0]
+    current_sum = 1
+
+    for i in range(1, len(param)):
+        if param[i] == param[i - 1]:
+            current_sum += 1
+            if (longest_so_far <= current_sum) and (param[i] > letter):
+                smallest_letter = param[i]
+                letter = param[i]
+            else:
+                letter = param[i]
+        else:
+            current_sum = 1
+        longest_so_far = max(longest_so_far, current_sum)
+
+    print(smallest_letter, longest_so_far)
+    return smallest_letter, longest_so_far
+
+
+assert longest_sequence("dghhhmhmx") == ("h", 3)
+assert longest_sequence("dhkkhhkkkt") == ("k", 3)
+assert longest_sequence("abbbadddadd") == ("b", 3)
